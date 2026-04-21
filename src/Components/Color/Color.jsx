bb14af7889a3431c38c3c47cfcf6cbc73680f169
oliviabelu <92735925+oliviabelu@users.noreply.github.com>
2026-04-21 16:36:56 +0200

    changed p to h2

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -4,7 +4,7 @@ export default function Color({ hex, role, contrastText }) {
   console.log(hex);
   return (
     <article className="color-card" style={{ backgroundColor: hex }}>
-      <p className="color-card-headline">{hex}</p>
+      <h2 className="color-card__headline">{hex}</h2>
       <p style={{ color: contrastText }}>{role}</p>
       <p style={{ color: contrastText }}>contrast: {contrastText}</p>
     </article>
