85373b30da81f253741c15450c6d7108f8883b59
Olivia <oliviabelu@web.de>
2026-04-21 16:44:08 +0200

    fontweight role

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -4,8 +4,10 @@ export default function Color({ hex, role, contrastText }) {
   console.log(hex);
   return (
     <article className="color-card" style={{ backgroundColor: hex }}>
-      <h2 className="color-card__headline">{hex}</h2>
-      <p style={{ color: contrastText }}>{role}</p>
+      <p className="color-card__headline">{hex}</p>
+      <p className="color-card__role" style={{ color: contrastText }}>
+        {role}
+      </p>
       <p style={{ color: contrastText }}>contrast: {contrastText}</p>
     </article>
   );
