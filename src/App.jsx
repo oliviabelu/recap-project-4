7dce4702cd519624495289db2e71021d2dcf8900
Olivia <oliviabelu@web.de>
2026-04-21 16:08:55 +0200

    display hex, role and contrastText; style cards; maps color cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 
+//const color1 = initialColors[0];
+
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      // display colors here
+      {initialColors.map((color) => (
+        <Color
+          key={color.id}
+          hex={color.hex}
+          role={color.role}
+          contrastText={color.contrastText}
+        ></Color>
+      ))}
     </>
   );
 }
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,12 @@
 import "./Color.css";
 
-export default function Color({ color }) {
-  return null;
+export default function Color({ hex, role, contrastText }) {
+  console.log(hex);
+  return (
+    <article className="color-card" style={{ backgroundColor: hex }}>
+      <p className="color-card-headline">{hex}</p>
+      <p style={{ color: contrastText }}>{role}</p>
+      <p style={{ color: contrastText }}>contrast: {contrastText}</p>
+    </article>
+  );
 }
