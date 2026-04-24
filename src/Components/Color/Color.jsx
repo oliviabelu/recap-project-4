cc29853b249175f9ea829d8dbf19a1a6730f9b37
Olivia <oliviabelu@web.de>
2026-04-24 10:37:14 +0200

    fetch api for contrast check

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -14,6 +14,7 @@ export default function Color({
   const [confirmation, setConfirmation] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [isCopying, setIsCopying] = useState(false);
+  const [contrastScore, setContrastScore] = useState("");
 
   function handleConfirmationSet(bool) {
     setConfirmation(bool);
@@ -50,6 +51,46 @@ export default function Color({
     }
   }, [isCopying]);
 
+  function handleContrastScore(newContrastScore) {
+    setContrastScore(newContrastScore);
+  }
+
+  useEffect(() => {
+    async function postFetch() {
+      const response = await fetch(
+        "https://www.aremycolorsaccessible.com/api/are-they",
+        {
+          method: "POST",
+          body: JSON.stringify({ colors: [hex, contrastText] }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      const contrastCheck = await response.json();
+
+      handleContrastScore(contrastCheck.overall);
+    }
+
+    postFetch();
+  }, [hex, contrastText]);
+
+  function getScoreBackground() {
+    switch (contrastScore) {
+      case "Yup":
+        return "#38c743";
+
+      case "Kinda":
+        return "#f8f807";
+
+      case "Nope":
+        return "#fd2b13";
+      default:
+        return "#c0c0c0";
+    }
+  }
+
   return (
     <article className="color-card" style={{ backgroundColor: hex }}>
       <h2 className="color-card__headline">{hex}</h2>
@@ -59,12 +100,18 @@ export default function Color({
       />
       <h3 style={{ color: contrastText }}>{role}</h3>
       <p style={{ color: contrastText }}>contrast: {contrastText}</p>
+      <p
+        className="color-card__contrast-score"
+        style={{ backgroundColor: getScoreBackground() }}
+      >
+        Overall Contrast Score: {contrastScore}
+      </p>
       {isEdit ? (
         <>
           <ColorForm
             onColorButton={handleEditColor}
             initialData={{ role: role, hex: hex, contrastText: contrastText }}
-            buttonName={"EDIT"}
+            buttonName={"UPDATE COLOR"}
           />
           <button type="button" onClick={handleIsEdit}>
             CANCEL
